9f9c3a5a4f3424342ab228d3e28bc3746c57a35d
m1992001 <lmq813@yeah.net>
2019-01-10 17:38:29 +0800

    聊天室接入。未完成

--- app/src/main/java/com/lmq/common/AppContact.java
+++ app/src/main/java/com/lmq/common/AppContact.java
@@ -12,7 +12,10 @@ public class AppContact {
     //https://github.com/m1992001/NewApp.git    adffdddde7d4358adc84963882da5e0c4f2c126c
     public static String FileStorePath="NewBrainApp";
     public static  String ImageFileStorePath="NewBrainApp/imgs/";
-    public static String localImAccount="acc_01";
-    public static String localImPwd="111111";
+//    public static String localImAccount="acc_01";
+    public static String localImAccount="acc01";
+    public static String localImPwd="96e79218965eb72c92a549dd5a330112";
+   // public static String localImPwd="111111";
     public static String localImDes="acc_02";
+    public static String IMAppkey="6b938454a1181c577a7d5d29972c84bc";
 }
